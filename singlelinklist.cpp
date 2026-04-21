03339955f2916291b73f62133f1c05be250f36ee
taufiq72 <taufiqq1207@gmail.com>
2026-04-21 11:53:37 +0700

    menambahkan bool del node

--- singlelinklist.cpp
+++ singlelinklist.cpp
@@ -77,5 +77,22 @@ class LinkedList
         return (*current != NULL);
     }
 
+    bool delNode(int nim)
+    {
+        Node *previous, *current;
+        if (!search(nim, &previous, &current))
+            return false;
+
+        if (current == START)
+        {
+            START = START->next;
+        }
+        else
+        {
+            previous->next = current->next;
+        }
+        delete current;
+        return true;
+    }
 
 };
