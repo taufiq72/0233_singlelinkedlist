df672f802223139707b92007f28514e5aaa0660c
taufiq72 <taufiqq1207@gmail.com>
2026-04-21 11:51:54 +0700

    menambahkan void addnote

--- singlelinklist.cpp
+++ singlelinklist.cpp
@@ -12,4 +12,52 @@ public:
 class LinkedList
 {
     Node*START;
-}
+
+    public:
+    LinkedList()
+    {
+        START = NULL;
+    }
+
+    void addNode()
+    {
+        int nim;
+        cout << "\nMasukkan NIM Mahasiswa: ";
+        cin >> nim;
+
+        Node *Nodebaru = new Node();
+        Nodebaru->noMhs = nim;
+
+        if (START == NULL || START->noMhs >= Nodebaru->noMhs)
+        {
+            if (START != NULL && nim == START->noMhs)
+            {
+                cout << "\nDuplikasi noMhs tidak di ijinkan\n";
+                return;
+            }
+
+            Nodebaru->next = START;
+            START = Nodebaru;
+            return;
+        }
+
+        Node *previous = START;
+        Node *current = START;
+
+        while (current != NULL && nim > current->noMhs)
+        {
+            if (nim == current->noMhs)
+            {
+                cout << "\nDuplikasi noMhs tidak di ijinkan\n";
+                return;
+            }
+            previous = current;
+            current = current->next;
+        }
+
+        Nodebaru->next = current;
+        previous->next = Nodebaru;
+    }
+
+
+};
